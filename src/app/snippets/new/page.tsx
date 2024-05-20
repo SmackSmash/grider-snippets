8bd5cf8e200681fb87c824320a3d00e641d8f5bc
Dan Moffat <danmoffat@gmail.com>
2024-05-20 20:47:09 +0100

    Tiny style change

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,7 +1,7 @@
 export default function SnippetCreatePage() {
   return (
     <form>
-      <h3 className='m-3 flex h-3 w-3 font-bold'>Create a snippet</h3>
+      <h3 className='m-3 font-bold'>Create a snippet</h3>
     </form>
   );
 }
