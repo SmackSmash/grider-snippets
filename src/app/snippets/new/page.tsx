d885c6857a6eff076d37aa8635db92591e70cfd2
Dan Moffat <danmoffat@gmail.com>
2024-05-20 20:53:30 +0100

    Coded up form

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -2,6 +2,20 @@ export default function SnippetCreatePage() {
   return (
     <form>
       <h3 className='m-3 font-bold'>Create a snippet</h3>
+      <div className='flex flex-col gap-4 p-3'>
+        <div className='flex gap-4'>
+          <label className='w-12' htmlFor='title'>
+            Title
+          </label>
+          <input className='w-full rounded border p-2' type='text' name='title' />
+        </div>
+        <div className='flex gap-4'>
+          <label className='w-12' htmlFor='code'>
+            Code
+          </label>
+          <textarea className='w-full rounded border p-2' name='code' />
+        </div>
+      </div>
     </form>
   );
 }
