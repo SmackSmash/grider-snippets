906e809ceca257e3229ab57fef5abf43682a84be
Dan Moffat <danmoffat@gmail.com>
2024-05-21 00:04:15 +0100

    Created a server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <h1>Hello world</h1>
+import { db } from '@/db';
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  return (
+    <div>
+      {snippets.map(({ id, title, code }) => (
+        <div key={id}>
+          <h1>{title}</h1>
+          <pre>{code}</pre>
+        </div>
+      ))}
+    </div>
+  );
 }
