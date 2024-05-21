8060f8bf1cc2c84601a2ca9883e670116d1621bc
Dan Moffat <danmoffat@gmail.com>
2024-05-21 20:04:42 +0100

    Added syntax highlighting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
+import { getHighlighter } from 'shiki';
+import parse from 'html-react-parser';
 import { db } from '@/db';
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
+
+  const highlighter = await getHighlighter({
+    themes: ['poimandres'],
+    langs: ['javascript']
+  });
 
   return (
     <div>
       <h1 className='text-3xl text-poimandres-darkpink'>Snippets</h1>
       {snippets.map(({ id, title, code }) => (
         <div className='my-4' key={id}>
-          <h1>{title}</h1>
-          <div className='bg-poimandres-darkslate p-2'>
-            <pre className='text-poimandres-yellow'>{code}</pre>
+          <h1 className='mb-2'>{title}</h1>
+          <div className='rounded bg-poimandres-blackslate p-4 shadow-md'>
+            {parse(
+              highlighter.codeToHtml(code, {
+                lang: 'javascript',
+                theme: 'poimandres'
+              })
+            )}
           </div>
         </div>
       ))}
