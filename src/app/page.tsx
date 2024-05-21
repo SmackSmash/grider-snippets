c3d6cd8e3bcde6215d21a0c5ca8fb10ee40c9403
Dan Moffat <danmoffat@gmail.com>
2024-05-21 18:43:48 +0100

    Little styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
     <div>
       <h1 className='text-3xl text-poimandres-darkpink'>Snippets</h1>
       {snippets.map(({ id, title, code }) => (
-        <div key={id}>
+        <div className='my-4' key={id}>
           <h1>{title}</h1>
           <div className='bg-poimandres-darkslate p-2'>
             <pre className='text-poimandres-yellow'>{code}</pre>
