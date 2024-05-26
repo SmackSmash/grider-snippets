dc1b22693a6ebbff1bb5926e1d9f00fe799dbcea
Dan Moffat <danmoffat@gmail.com>
2024-05-26 17:29:41 +0100

    A little styling

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -13,24 +13,19 @@ export default function SnippetCreatePage() {
   return (
     <form className='font-sans' action={createSnippet}>
       <div className='flex flex-col gap-4 p-3'>
-        <div className='flex gap-4'>
-          <label className='w-12' htmlFor='title'>
-            Title
-          </label>
-          <input
-            className='w-full rounded border p-2 text-poimandres-blackslate'
-            type='text'
-            name='title'
-          />
-        </div>
-        <div className='flex gap-4'>
-          <label className='w-12' htmlFor='code'>
-            Code
-          </label>
-          <textarea className='w-full rounded border p-2 text-poimandres-blackslate' name='code' />
-        </div>
+        <label htmlFor='title'>Title</label>
+        <input
+          className='w-full rounded border border-none bg-poimandres-blackslate p-2 font-mono text-poimandres-white outline-none'
+          type='text'
+          name='title'
+        />
+        <label htmlFor='code'>Code</label>
+        <textarea
+          className='min-h-80 w-full rounded border border-none bg-poimandres-blackslate p-2 font-mono text-poimandres-white outline-none'
+          name='code'
+        />
         <button
-          className='rounded bg-poimandres-darkblue p-2 font-bold uppercase text-poimandres-darkslate hover:text-poimandres-white'
+          className='rounded bg-poimandres-lightgreen p-2 font-poetsen font-bold text-poimandres-darkslate'
           type='submit'
         >
           Add Snippet
