5110ad8de319b8e4d9a78df187b3331d93ffa634
Dan Moffat <danmoffat@gmail.com>
2024-05-27 20:49:09 +0100

    Added a nice little loading spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,12 @@
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div
+      className='text-surface mx-auto inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-poimandres-lightgreen'
+      role='status'
+    >
+      <span className='absolute -m-px h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 [clip:rect(0,0,0,0)]'>
+        Loading...
+      </span>
+    </div>
+  );
 }
