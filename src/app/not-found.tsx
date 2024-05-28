f389fd1d10d87ebcaa10bdd0534464aaba4888c0
Dan Moffat <danmoffat@gmail.com>
2024-05-29 00:48:07 +0100

    Added not found logic to edit page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className='flex h-full w-full items-center justify-center'>
-      <h2 className='text-4xl text-poimandres-darkpink'>404 | Not found</h2>
+      <h2 className='text-4xl text-poimandres-lightblue'>404 | Not found</h2>
     </div>
   );
 }
--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,9 +1,18 @@
+import { notFound } from 'next/navigation';
+import { db } from '@/db';
+
 interface EditSnipppetPageProps {
   params: {
     id: string;
   };
 }
 
-export default function EditSnippetPage({ params: { id } }: EditSnipppetPageProps) {
+export default async function EditSnippetPage({ params: { id } }: EditSnipppetPageProps) {
+  const snippet = await db.snippet.findFirst({ where: { id: Number(id) } });
+
+  if (!snippet) {
+    return notFound();
+  }
+
   return <div>Edit snippet page for {id}</div>;
 }
