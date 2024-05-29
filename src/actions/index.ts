587fea807cfe4aaf22241a194f220e023a383cf3
Dan Moffat <danmoffat@gmail.com>
2024-05-29 02:32:59 +0100

    Added redirect... nice

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,8 @@
 'use server';
+import { redirect } from 'next/navigation';
 import { db } from '@/db';
 
 export async function updateSnippet(id: number, code: string | undefined) {
   await db.snippet.update({ where: { id }, data: { code } });
+  redirect('/');
 }
