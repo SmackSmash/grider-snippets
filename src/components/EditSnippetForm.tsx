9ae919bb59010d8c16af3e84d6c8944b517d0f24
Dan Moffat <danmoffat@gmail.com>
2024-05-29 01:11:51 +0100

    Added monaco editor to edit snippet page

--- src/components/EditSnippetForm.tsx
+++ src/components/EditSnippetForm.tsx
@@ -1,12 +1,18 @@
 'use client';
 import { Snippet } from '@prisma/client';
+import Editor from '@monaco-editor/react';
 
 interface EditSnippetFormProps {
   snippet: Snippet;
 }
 
 const EditSnippetForm = ({ snippet: { id, title, code } }: EditSnippetFormProps) => {
-  return <div>Edit snippet form for &quot;{title}&quot;</div>;
+  return (
+    <div>
+      <h1>Edit snippet form for &quot;{title}&quot;</h1>
+      <Editor height='400px' defaultLanguage='javascript' defaultValue={code} />
+    </div>
+  );
 };
 
 export default EditSnippetForm;
