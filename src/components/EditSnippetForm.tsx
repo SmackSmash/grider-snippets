878710003333c7ee9d4671d083967dfe229fe444
Dan Moffat <danmoffat@gmail.com>
2024-05-29 01:18:02 +0100

    Added dark theme for monaco editor

--- src/components/EditSnippetForm.tsx
+++ src/components/EditSnippetForm.tsx
@@ -10,7 +10,7 @@ const EditSnippetForm = ({ snippet: { id, title, code } }: EditSnippetFormProps)
   return (
     <div>
       <h1>Edit snippet form for &quot;{title}&quot;</h1>
-      <Editor height='400px' defaultLanguage='javascript' defaultValue={code} />
+      <Editor height='400px' defaultLanguage='javascript' theme='vs-dark' defaultValue={code} />
     </div>
   );
 };
